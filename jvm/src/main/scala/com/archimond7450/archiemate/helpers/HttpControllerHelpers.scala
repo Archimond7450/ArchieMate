87019046d7cd584c9499458811b142fc3367837c
Miroslav Horák <warcallerthefirst@gmail.com>
2025-09-23 12:16:42 +0200

    Improve JWT handling with automatic age extension

--- jvm/src/main/scala/com/archimond7450/archiemate/helpers/HttpControllerHelpers.scala
+++ jvm/src/main/scala/com/archimond7450/archiemate/helpers/HttpControllerHelpers.scala
@@ -1,13 +1,86 @@
 package com.archimond7450.archiemate.helpers
 
+import com.archimond7450.archiemate.extensions.Settings
 import org.apache.pekko.http.scaladsl.model.StatusCodes
-import org.apache.pekko.http.scaladsl.model.headers.HttpCookiePair
+import org.apache.pekko.http.scaladsl.model.headers.{HttpCookie, HttpCookiePair}
 import org.apache.pekko.http.scaladsl.server.Directives.*
 import org.apache.pekko.http.scaladsl.server.Route
 
 object HttpControllerHelpers {
-  def failWithoutSessionCookie(route: HttpCookiePair => Route): Route = optionalCookie("session") {
-    case Some(jwt) => route(jwt)
-    case None => complete(StatusCodes.Unauthorized)
+  def failWithoutSessionCookie(
+      innerRoute: HttpCookiePair => Route
+  )(using settings: Settings): Route = {
+    optionalCookie("session") {
+      case Some(jwt) =>
+        updateSessionCookie(jwt) { newJwt =>
+          innerRoute(newJwt)
+        }
+      case None => complete(StatusCodes.Unauthorized)
+    }
+  }
+
+  def logoutSessionCookie(
+      innerRoute: => Route
+  )(using settings: Settings): Route = {
+    optionalCookie("session") { jwtOption =>
+      expireOptionSessionCookie(jwtOption) {
+        innerRoute
+      }
+    }
+  }
+
+  def expireSessionCookie(jwt: HttpCookiePair)(
+      innerRoute: => Route
+  ): Route = {
+    setCookie(jwt.toCookie.withMaxAge(0)) {
+      innerRoute
+    }
+  }
+
+  def expireOptionSessionCookie(
+      jwtOption: Option[HttpCookiePair]
+  )(innerRoute: => Route)(using settings: Settings): Route = {
+    jwtOption match {
+      case Some(jwt) =>
+        expireSessionCookie(jwt) {
+          innerRoute
+        }
+      case None =>
+        setCookie(
+          HttpCookie("session", "")
+            .withPath("/")
+            .withSecure(settings.secureCookies)
+            .withHttpOnly(true)
+            .withMaxAge(0)
+        ) {
+          innerRoute
+        }
+    }
+  }
+
+  def createSessionCookie(jwt: String)(innerRoute: => Route)(using
+      settings: Settings
+  ): Route = {
+    setCookie(
+      HttpCookie("session", jwt)
+        .withPath("/")
+        .withSecure(settings.secureCookies)
+        .withHttpOnly(true)
+        .withMaxAge(settings.jwtExpiration.toSeconds)
+    ) {
+      innerRoute
+    }
+  }
+
+  def updateSessionCookie(jwt: HttpCookiePair)(
+      innerRoute: HttpCookiePair => Route
+  )(using settings: Settings): Route = {
+    val updatedCookie =
+      jwt.toCookie.withMaxAge(settings.jwtExpiration.toSeconds)
+    setCookie(
+      updatedCookie
+    ) {
+      innerRoute(HttpCookiePair(updatedCookie.name, updatedCookie.value))
+    }
   }
 }
--- jvm/src/main/scala/com/archimond7450/archiemate/http/api/v1/UserController.scala
+++ jvm/src/main/scala/com/archimond7450/archiemate/http/api/v1/UserController.scala
@@ -4,6 +4,7 @@ import com.archimond7450.archiemate.actors.ArchieMateMediator
 import com.archimond7450.archiemate.actors.services.JWTService
 import com.archimond7450.archiemate.actors.services.controllerhelpers.UserControllerHelperService
 import com.archimond7450.archiemate.actors.twitch.api.TwitchApiClient
+import com.archimond7450.archiemate.extensions.Settings
 import com.archimond7450.archiemate.helpers.HttpControllerHelpers.failWithoutSessionCookie
 import com.archimond7450.archiemate.http.IController
 import com.archimond7450.archiemate.http.User.UserResponse
@@ -18,27 +19,49 @@ import org.apache.pekko.util.Timeout
 
 import scala.util.{Failure, Success}
 
-final class UserController(using mediator: ActorRef[ArchieMateMediator.Command])(using Scheduler, Timeout) extends IController("user") with FailFastCirceSupport {
+final class UserController(using
+    mediator: ActorRef[ArchieMateMediator.Command]
+)(using Settings, Scheduler, Timeout)
+    extends IController("user")
+    with FailFastCirceSupport {
   override def routes: Route = extractLog { log =>
     given LoggingAdapter = log
     info
   }
 
-  private def info(using log: LoggingAdapter): Route = (get & pathEndOrSingleSlash) {
-    failWithoutSessionCookie { jwt =>
-      onComplete(mediator.ask[UserControllerHelperService.GetUserResponse](ref => ArchieMateMediator.SendUserControllerHelperServiceCommand(UserControllerHelperService.GetUser(ref, jwt.value)))) {
-        case Success(UserControllerHelperService.GetUserOKResponse(Success(user))) =>
-          complete(UserResponse(user.id, user.login, user.display_name, user.profile_image_url))
+  private def info(using log: LoggingAdapter): Route =
+    (get & pathEndOrSingleSlash) {
+      failWithoutSessionCookie { jwt =>
+        onComplete(
+          mediator.ask[UserControllerHelperService.GetUserResponse](ref =>
+            ArchieMateMediator.SendUserControllerHelperServiceCommand(
+              UserControllerHelperService.GetUser(ref, jwt.value)
+            )
+          )
+        ) {
+          case Success(
+                UserControllerHelperService.GetUserOKResponse(Success(user))
+              ) =>
+            complete(
+              UserResponse(
+                user.id,
+                user.login,
+                user.display_name,
+                user.profile_image_url
+              )
+            )
 
-        case Success(UserControllerHelperService.GetUserOKResponse(Failure(ex))) =>
-          complete(StatusCodes.InternalServerError)
+          case Success(
+                UserControllerHelperService.GetUserOKResponse(Failure(ex))
+              ) =>
+            complete(StatusCodes.InternalServerError)
 
-        case Success(UserControllerHelperService.InvalidJWT) =>
-          complete(StatusCodes.Unauthorized)
+          case Success(UserControllerHelperService.InvalidJWT) =>
+            complete(StatusCodes.Unauthorized)
 
-        case Failure(ex) =>
-          complete(StatusCodes.InternalServerError)
+          case Failure(ex) =>
+            complete(StatusCodes.InternalServerError)
+        }
       }
     }
-  }
 }
