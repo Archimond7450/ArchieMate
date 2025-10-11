9fb82d90345f9daa611d0e0bec972ee01e1186b8
Miroslav Horák <warcallerthefirst@gmail.com>
2025-10-11 22:46:18 +0200

    Unit tests added for BasicChatbotSettingsRepository

---
+++ jvm/src/test/scala/com/archimond7450/archiemate/actors/repositories/settings/BasicChatbotSettingsRepositorySpec.scala
@@ -0,0 +1,165 @@
+package com.archimond7450.archiemate.actors.repositories.settings
+
+import com.archimond7450.archiemate.actors.ArchieMateMediator
+import com.archimond7450.archiemate.actors.chatbot.TwitchChatbotsSupervisor
+import com.archimond7450.archiemate.http.ChannelSettings.BasicChatbotSettings
+import org.apache.pekko.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.apache.pekko.actor.typed.ActorRef
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class BasicChatbotSettingsRepositorySpec
+    extends ScalaTestWithActorTestKit
+    with AnyWordSpecLike {
+  import BasicChatbotSettingsRepositorySpec.*
+
+  "A Basic Chatbot Settings Repository" should {
+    val twitchChatbotsSupervisorProbe =
+      testKit.createTestProbe[TwitchChatbotsSupervisor.Command](
+        TwitchChatbotsSupervisor.actorName
+      )
+    given twitchChatbotsSupervisor: ActorRef[TwitchChatbotsSupervisor.Command] =
+      twitchChatbotsSupervisorProbe.ref
+    val mediatorProbe = testKit.createTestProbe[ArchieMateMediator.Command](
+      ArchieMateMediator.actorName
+    )
+    given mediator: ActorRef[ArchieMateMediator.Command] = mediatorProbe.ref
+
+    "return default settings when GetSettings is received for any twitchRoomId if initially started" in {
+      val basicChatbotSettingsRepository = testKit.spawn(
+        BasicChatbotSettingsRepository(),
+        s"${BasicChatbotSettingsRepository.actorName}0"
+      )
+      val probe = testKit.createTestProbe[BasicChatbotSettings]()
+
+      for (roomId <- Seq(firstRoomId, secondRoomId, thirdRoomId)) {
+        basicChatbotSettingsRepository ! BasicChatbotSettingsRepository
+          .GetSettings(
+            probe.ref,
+            roomId
+          )
+        probe.expectMessage(defaultSettings)
+      }
+
+      mediatorProbe.expectNoMessage()
+      twitchChatbotsSupervisorProbe.expectNoMessage()
+    }
+
+    "return acknowledge message when settings change and notify twitch chatbots supervisor of the change" in {
+      val basicChatbotSettingsRepository = testKit.spawn(
+        BasicChatbotSettingsRepository(),
+        s"${BasicChatbotSettingsRepository.actorName}1"
+      )
+      val probe = testKit
+        .createTestProbe[BasicChatbotSettingsRepository.Acknowledged.type]()
+
+      for (roomId <- Seq(firstRoomId, thirdRoomId)) {
+        basicChatbotSettingsRepository ! BasicChatbotSettingsRepository
+          .ChangeSettings(
+            probe.ref,
+            roomId,
+            newSettings
+          )
+        probe.expectMessage(BasicChatbotSettingsRepository.Acknowledged)
+
+        val mediatorCmd = mediatorProbe.expectMessageType[
+          ArchieMateMediator.SendTwitchChatbotsSupervisorCommand
+        ]
+        twitchChatbotsSupervisor ! mediatorCmd.cmd
+        twitchChatbotsSupervisorProbe.expectMessage(
+          TwitchChatbotsSupervisor.NewChannelSettingsEvent(
+            roomId,
+            TwitchChatbotsSupervisor.BasicChatbotSettingsChanged(newSettings)
+          )
+        )
+      }
+
+      mediatorProbe.expectNoMessage()
+      twitchChatbotsSupervisorProbe.expectNoMessage()
+    }
+
+    "return default settings when settings are not present for twitchRoomId, otherwise return the stored settings when GetSettings message is received" in {
+      val basicChatbotSettingsRepository = testKit.spawn(
+        BasicChatbotSettingsRepository(),
+        s"${BasicChatbotSettingsRepository.actorName}2"
+      )
+      val probe = testKit.createTestProbe[BasicChatbotSettings]()
+
+      for (roomId <- Seq(firstRoomId, secondRoomId, thirdRoomId)) {
+        basicChatbotSettingsRepository ! BasicChatbotSettingsRepository
+          .GetSettings(
+            probe.ref,
+            roomId
+          )
+        probe.expectMessage(
+          if (roomId == secondRoomId) defaultSettings else newSettings
+        )
+      }
+
+      mediatorProbe.expectNoMessage()
+      twitchChatbotsSupervisorProbe.expectNoMessage()
+    }
+
+    "return acknowledge message when settings reset and notify twitch chatbots supervisor of the change" in {
+      val basicChatbotSettingsRepository = testKit.spawn(
+        BasicChatbotSettingsRepository(),
+        s"${BasicChatbotSettingsRepository.actorName}3"
+      )
+      val probe = testKit
+        .createTestProbe[BasicChatbotSettingsRepository.Acknowledged.type]()
+
+      val roomId = thirdRoomId
+
+      basicChatbotSettingsRepository ! BasicChatbotSettingsRepository
+        .ResetSettings(
+          probe.ref,
+          roomId
+        )
+      probe.expectMessage(BasicChatbotSettingsRepository.Acknowledged)
+
+      val mediatorCmd = mediatorProbe.expectMessageType[
+        ArchieMateMediator.SendTwitchChatbotsSupervisorCommand
+      ]
+      twitchChatbotsSupervisor ! mediatorCmd.cmd
+      twitchChatbotsSupervisorProbe.expectMessage(
+        TwitchChatbotsSupervisor.NewChannelSettingsEvent(
+          roomId,
+          TwitchChatbotsSupervisor.BasicChatbotSettingsChanged(defaultSettings)
+        )
+      )
+
+      mediatorProbe.expectNoMessage()
+      twitchChatbotsSupervisorProbe.expectNoMessage()
+    }
+
+    "return default settings when settings are not present for twitchRoomId, otherwise return the stored settings when GetSettings message is received #2" in {
+      val basicChatbotSettingsRepository = testKit.spawn(
+        BasicChatbotSettingsRepository(),
+        s"${BasicChatbotSettingsRepository.actorName}4"
+      )
+      val probe = testKit.createTestProbe[BasicChatbotSettings]()
+
+      for (roomId <- Seq(firstRoomId, secondRoomId, thirdRoomId)) {
+        basicChatbotSettingsRepository ! BasicChatbotSettingsRepository
+          .GetSettings(
+            probe.ref,
+            roomId
+          )
+        probe.expectMessage(
+          if (roomId == firstRoomId) newSettings else defaultSettings
+        )
+      }
+
+      mediatorProbe.expectNoMessage()
+      twitchChatbotsSupervisorProbe.expectNoMessage()
+    }
+  }
+}
+
+object BasicChatbotSettingsRepositorySpec {
+  val firstRoomId = "1"
+  val secondRoomId = "2"
+  val thirdRoomId = "3"
+
+  val defaultSettings = BasicChatbotSettings()
+  val newSettings = BasicChatbotSettings(join = true)
+}
